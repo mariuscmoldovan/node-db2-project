3ace0a022b6eb88c4cb72cefd50eeb089ca060d2
mariusmoldovan <biris_21@yahoo.com>
2021-07-13 21:37:02 -0500

    migration up created

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,5 +1,13 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable("cars", table => {
+      table.increments();
+      table.text("vin", 17).unique().notNullable();
+      table.text("make", 120).notNullable();
+      table.text("model", 120).notNullable();
+      table.decimal("mileage").unsigned().notNullable();
+      table.text("title")
+      table.text("transmission")
+});
 };
 
 exports.down = function (knex) {
